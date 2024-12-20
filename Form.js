7a72fa9116358a2c1d19d8db2137ac71596ab9b1
wendsaw <sawadogo.edgard@gmail.com>
2024-12-19 21:26:37 -0700

    update4

--- Form.js
+++ Form.js
@@ -1,15 +1,51 @@
 
 
-function Randomword(Ntry) {
+let rword = document.querySelector('.btm');
+//    console.log(rword.textContent);
+let wordbox = document.querySelector('wordform');
+
+
+
+
+
+
+
+function Randomword(e) {
     const chars = ["escapology", "brightwork", "verkrampte", "protectrix", "nudibranch", "grandchild", "newfangled", "flugelhorn", "mythologer", "pluperfect", "jellygraph", "quickthorn", "rottweiler", "technician", "cowpuncher", "middlebrow", "jackhammer", "triphthong", "wunderkind", "dazzlement", "jabberwock", "witchcraft", "pawnbroker", "thumbprint", "motorcycle", "cryptogram", "torchlight", "bankruptcy"];
     let result = "";
-    for (i = 0; i < Ntry; i++) {
+    // for (i = 0; i < Ntry; i++) {
 
-        result = chars[Math.floor(Math.random() * chars.length)]
-        // console.log(result);
-    
-    }
+    result = chars[Math.floor(Math.random() * chars.length)]
+//hide character to display
+
+e.target.textContent = result
+
+    // rword.textContent = result
+
+
+
+    // }
     return result;
+
 }
 
+function hideword(newword){
+
+Randomword()
+
+}
+
+function checkword(Cw){
+
+    hideword()
+}
+
+
+
+
+
+
+
+
+rword.addEventListener('click', Randomword);
 
