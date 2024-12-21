93c4601b0de4a4ab796b7237763efbf9605771b2
wendsaw <sawadogo.edgard@gmail.com>
2024-12-20 19:45:41 -0700

    update6

--- Form.js
+++ Form.js
@@ -2,6 +2,11 @@
 
 let nAccount = document.querySelector('#newaccount');
 let login = document.querySelector('#login');
+let gener = document.querySelector('#wordbox');
+let word = document.getElementById('wordbox');
+
+
+
 
 // console.log(nAccount);
 
@@ -9,26 +14,23 @@ let login = document.querySelector('#login');
 
 function cChange(e) {
     e.target.style.backgroundColor = 'red';
+    e.stopPropagation();
     return e
+
 }
 
 document.body.style.backgroundColor = 'green'
-let rword = document.querySelector('.btm');
 
-//console.log(rword.textContent);
-let wordbox = document.querySelector('wordform');
 
 //this function generate random word for the array chars
+const chars = ["escapology", "brightwork", "verkrampte", "protectrix", "nudibranch", "grandchild", "newfangled", "flugelhorn", "mythologer", "pluperfect", "jellygraph", "quickthorn", "rottweiler", "technician", "cowpuncher", "middlebrow", "jackhammer", "triphthong", "wunderkind", "dazzlement", "jabberwock", "witchcraft", "pawnbroker", "thumbprint", "motorcycle", "cryptogram", "torchlight", "bankruptcy"];
 
 function Randomword(e) {
-    const chars = ["escapology", "brightwork", "verkrampte", "protectrix", "nudibranch", "grandchild", "newfangled", "flugelhorn", "mythologer", "pluperfect", "jellygraph", "quickthorn", "rottweiler", "technician", "cowpuncher", "middlebrow", "jackhammer", "triphthong", "wunderkind", "dazzlement", "jabberwock", "witchcraft", "pawnbroker", "thumbprint", "motorcycle", "cryptogram", "torchlight", "bankruptcy"];
+    
     let result = "";
     // for (i = 0; i < Ntry; i++) {
-
     result = chars[Math.floor(Math.random() * chars.length)]
-    
     return result;
-
 }
 
 //this function hide some string for the player to guess
@@ -40,31 +42,19 @@ function hideword(e) {
     newword = word.slice(0, 3);
     newword = newword.toUpperCase();
     console.log(newword);
-    e.target.textContent = newword;
-
+    
+    e.target
     return e
 
 }
 
 // function checkword() {
-// let guessword=''
-// Randomword();
 
-//     for (i=0; i<3; i++){
-//         if (guessword==word),
+//  }
 
 
 
-//     }
-
-
-// }
-
-
-
-
-
-rword.addEventListener('click', hideword);
+gener.addEventListener('click', hideword);
 nAccount.addEventListener('click', cChange);
 login.addEventListener('click', cChange);
 
