dab5392e4b727d84cde2e029f70c649af93e765e
wendsaw <sawadogo.edgard@gmail.com>
2024-12-20 18:27:12 -0700

    update5

--- Form.js
+++ Form.js
@@ -1,14 +1,24 @@
 
 
+let nAccount = document.querySelector('#newaccount');
+let login = document.querySelector('#login');
+
+// console.log(nAccount);
+
+//this fucntion change the color of the bottom to red when user click
+
+function cChange(e) {
+    e.target.style.backgroundColor = 'red';
+    return e
+}
+
+document.body.style.backgroundColor = 'green'
 let rword = document.querySelector('.btm');
-//    console.log(rword.textContent);
+
+//console.log(rword.textContent);
 let wordbox = document.querySelector('wordform');
 
-
-
-
-
-
+//this function generate random word for the array chars
 
 function Randomword(e) {
     const chars = ["escapology", "brightwork", "verkrampte", "protectrix", "nudibranch", "grandchild", "newfangled", "flugelhorn", "mythologer", "pluperfect", "jellygraph", "quickthorn", "rottweiler", "technician", "cowpuncher", "middlebrow", "jackhammer", "triphthong", "wunderkind", "dazzlement", "jabberwock", "witchcraft", "pawnbroker", "thumbprint", "motorcycle", "cryptogram", "torchlight", "bankruptcy"];
@@ -16,59 +26,46 @@ function Randomword(e) {
     // for (i = 0; i < Ntry; i++) {
 
     result = chars[Math.floor(Math.random() * chars.length)]
-
-
-
-
-
-    //hide character to display
-
-    // textContent = result
-
-    // rword.textContent = result
-
-
-
-    // }
-
-   
+    
     return result;
-
-
 
 }
 
-let word = [];
-word = Randomword();
-console.log(word);
-word=word.slice(0, 3);
-word=word.toUpperCase();
-console.log(word);
-
-
+//this function hide some string for the player to guess
 function hideword(e) {
-
+    let newword = [];
     let word = [];
     word = Randomword();
     console.log(word);
-    word=word.slice(0, 3);
-    word=word.toUpperCase();
-    console.log(word);
-    
-    e.target.textContent = word;
+    newword = word.slice(0, 3);
+    newword = newword.toUpperCase();
+    console.log(newword);
+    e.target.textContent = newword;
 
-
+    return e
 
 }
 
-function checkword() {
+// function checkword() {
+// let guessword=''
+// Randomword();
 
-    hideword()
-}
+//     for (i=0; i<3; i++){
+//         if (guessword==word),
+
+
+
+//     }
+
+
+// }
 
 
 
 
 
 rword.addEventListener('click', hideword);
+nAccount.addEventListener('click', cChange);
+login.addEventListener('click', cChange);
+
 
