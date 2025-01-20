6ade988afa48a0cfb2b26250e411bc23aff40aff
wendsaw <sawadogo.edgard@gmail.com>
2025-01-20 03:40:05 -0700

    review5

--- Form.js
+++ Form.js
@@ -3,38 +3,60 @@
 //this function generate random word for the array chars
 const guessword=document.querySelector('#wordbox')
 const btm=document.querySelector('#btm')
-console.log(guessword);
-console.log(btm);
+const CorrectWord=document.querySelector('#gword')
+const smt=document.querySelector('#smt')
+const number=document.querySelector('#Ntry')
+console.log(number.value);
+
+console.log(CorrectWord);
+
+
+// console.log(guessword);
+// console.log(btm);
 
  guessword.addEventListener('click',e=>{
     e.target
-    console.log('hello world');
-    word = Randomword();
-    console.log(`the word is ${word}`);
-    const c=word.split("")
+    let word = Randomword();
+    wordTrunk= hideword(word)
+    e.target.value=wordTrunk;
+    smt.addEventListener('click',e =>{
+        e.preventDefault()
+        console.log(CorrectWord.value);
+        result=cWord(word,CorrectWord.value)
+        if (result=='won'){
+            console.log(`you are a winner`);
+        }if (result=='lost'){
+            console.log(`you have ${number.value} trial left`);
+        }
+    })
+    
+ })
+
+
+function Randomword() {
+    const chars = ["escapology", "brightwork", "verkrampte", "protectrix", "nudibranch", "grandchild", "newfangled", "flugelhorn", "mythologer", "pluperfect", "jellygraph", "quickthorn", "rottweiler", "technician", "cowpuncher", "middlebrow", "jackhammer", "triphthong", "wunderkind", "dazzlement", "jabberwock", "witchcraft", "pawnbroker", "thumbprint", "motorcycle", "cryptogram", "torchlight", "bankruptcy"];
+    i = chars[Math.floor(Math.random() * chars.length)]
+    console.log(i);
+    return i
+}
+
+function hideword(word) {
+const c=word.split("")
     c[0]=''
     c[2]=''
     c[3]=''
     word=c.join('').toUpperCase()
-    e.target.value=word;
+    return word
+}
+function cWord(word,guessword){
     
 
- })
-
-function Randomword() {
-    const chars = ["escapology", "brightwork", "verkrampte", "protectrix", "nudibranch", "grandchild", "newfangled", "flugelhorn", "mythologer", "pluperfect", "jellygraph", "quickthorn", "rottweiler", "technician", "cowpuncher", "middlebrow", "jackhammer", "triphthong", "wunderkind", "dazzlement", "jabberwock", "witchcraft", "pawnbroker", "thumbprint", "motorcycle", "cryptogram", "torchlight", "bankruptcy"];
-
-    i = chars[Math.floor(Math.random() * chars.length)]
-    console.log(i);
-    return i
+    if (word==guessword){
+        return "won"
+    
+    }
+    else 
+   return "lost"
     
 }
 
-// function hideword(word) {
-    
-    
-    // return word
-
-// }
-
-// hideword();
