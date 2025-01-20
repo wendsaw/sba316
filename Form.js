0de5cd08111cd128b82f0134b1b3371a0f00ce04
wendsaw <sawadogo.edgard@gmail.com>
2025-01-19 23:00:50 -0700

    review1

--- Form.js
+++ Form.js
@@ -1,60 +1,30 @@
 
 
-let nAccount = document.querySelector('#newaccount');
-let login = document.querySelector('#login');
-let gener = document.querySelector('#wordbox');
-let word = document.getElementById('wordbox');
-
-
-
-// console.log(nAccount);
-
-//this fucntion change the color of the bottom to red when user click
-
-function cChange(e) {
-    e.target.style.backgroundColor = 'red';
-    e.stopPropagation();
-    return e
-
-}
-
-document.body.style.backgroundColor = 'green'
-
-
 //this function generate random word for the array chars
-const chars = ["escapology", "brightwork", "verkrampte", "protectrix", "nudibranch", "grandchild", "newfangled", "flugelhorn", "mythologer", "pluperfect", "jellygraph", "quickthorn", "rottweiler", "technician", "cowpuncher", "middlebrow", "jackhammer", "triphthong", "wunderkind", "dazzlement", "jabberwock", "witchcraft", "pawnbroker", "thumbprint", "motorcycle", "cryptogram", "torchlight", "bankruptcy"];
 
-function Randomword(e) {
+
+function Randomword() {
+    const chars = ["escapology", "brightwork", "verkrampte", "protectrix", "nudibranch", "grandchild", "newfangled", "flugelhorn", "mythologer", "pluperfect", "jellygraph", "quickthorn", "rottweiler", "technician", "cowpuncher", "middlebrow", "jackhammer", "triphthong", "wunderkind", "dazzlement", "jabberwock", "witchcraft", "pawnbroker", "thumbprint", "motorcycle", "cryptogram", "torchlight", "bankruptcy"];
+
+    i = chars[Math.floor(Math.random() * chars.length)]
+    console.log(i);
+    return i
     
-    let result = "";
-    // for (i = 0; i < Ntry; i++) {
-    result = chars[Math.floor(Math.random() * chars.length)]
-    return result;
 }
 
-//this function hide some string for the player to guess
-function hideword(e) {
-    let newword = [];
-    let word = [];
+// function hideword(word) {
+    
     word = Randomword();
+    console.log(`the word is ${word}`);
+    const c=word.split("")
+    c[0]=''
+    c[2]=''
+    c[3]=''
+    word=c.join('').toUpperCase()
+    console.log(c);
     console.log(word);
-    newword = word.slice(0, 3);
-    newword = newword.toUpperCase();
-    console.log(newword);
-    
-    e.target
-    return e
+    // return word
 
-}
+// }
 
-// function checkword() {
-
-//  }
-
-
-
-gener.addEventListener('click', hideword);
-nAccount.addEventListener('click', cChange);
-login.;
-
-
+// hideword();
